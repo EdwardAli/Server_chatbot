20c5e1bb4d8a66afd0645756f47acafb6cbeced0
EdwardAli <48312175+EdwardAli@users.noreply.github.com>
2022-04-12 04:40:44 +0200

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'production';
 
-const config = require(path.join(__dirname, '../../', 'config', 'config.js'))[env];
+const config = require(path.join(__dirname, '../../', 'config', 'config.json'))[env];
 
 const db = {};
 
